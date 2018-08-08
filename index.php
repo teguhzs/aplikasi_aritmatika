122f2d975f48209615d7084fbf5c740a6d683e2f
teguhzs <teguhs050196@gmail.com>
2018-08-08 13:21:42 +0700

    Add files via upload
    Perbaikan terhadap hasil terbilang dengan suara

--- index.php
+++ index.php
@@ -3,31 +3,45 @@
 <head>
  <title>Aplikasi Aritmatika Penambahan, Pengurangan, Perkalian Dan Pembagian</title>
  <script src='https://code.responsivevoice.org/responsivevoice.js'></script>
+ <!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+<!-- Optional theme -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+<!-- Latest compiled and minified JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
 </head>
 <body>
     <?PHP
         error_reporting(0);
 
-        echo '<h1 align="center">Aplikasi Aritmatika Penjumlahan, Pengurangan, Perkalian Dan Pembagian</h1>';
+        echo '<h1 align="center">Kalkulator</h1>';
 
 
          if(!$_POST['aksi'])
          {      
          /*-----Menampilkan Form Pengisian----*/
           echo '
-         <form method="post" action="'.$_SERVER['PHP_SELF'].'">
+          <div class="container" >
+         <form class="" method="post" action="'.$_SERVER['PHP_SELF'].'">
          <div align="center">
-          <input type="text" name="angka1" size="5">
-          <select name="aksi">
+         <label>Masukkan bilangan pertama</label>
+          <input type="number" class="form-control" name="angka1" size="" required>
+          <br>
+          <select class="form-control" name="aksi">
            <option value="tambah"> + </option>
            <option value="kurang"> - </option>
            <option value="bagi"> / </option>
            <option value="kali"> * </option>
           </select>
-          <input type="text" name="angka2" size="5">
-          <input type="submit" name="hitung" value="hitung">
+          <br>
+          <label>Masukkan bilangan kedua</label>
+          <input type="number" class="form-control" name="angka2" size="5" required>
+          <br>
+          <input type="submit" class="btn btn-success" name="hitung" value="hitung">
           </div>
          </form>
+         </div>
          ';
          }else{
          /*-----memasukan nilai ke variabel----*/
@@ -39,26 +53,28 @@
           {
            $hasil = $angka1 + $angka2;
            $operasi = '+';
+           $op_terbilang = ' Ditambah '; 
           }
           if($_POST['aksi'] == 'kurang')
           {
            $hasil = $angka1 - $angka2;
            $operasi = '-';
+           $op_terbilang = ' Dikurang '; 
+
           }
           if($_POST['aksi'] == 'bagi')
           {
            $hasil = $angka1 / $angka2;
            $operasi = '/';
+           $op_terbilang = ' Dibagi '; 
+
           }
           if($_POST['aksi'] == 'kali')
           {
            $hasil = $angka1 * $angka2;
            $operasi = 'X';
-          }
-          if($_POST['aksi'] == 'persen')
-          {
-           $hasil = (($angka2 * $angka1)/100);
-           $operasi = '% dari';
+           $op_terbilang = ' Dikali '; 
+
           }
 
  /*-----------Fungsi Hasil terbilang...........*/ 
@@ -107,7 +123,7 @@
 <script type="text/javascript">
   function play (){
    responsiveVoice.speak(
-    "<?php echo $hasil; ?>",
+    "Hasil perhitungan <?php  echo  $angka1.' '.$op_terbilang.' '.$angka2.' = '.$hasil; ?>",
     "Indonesian Female",
    );
   }
@@ -119,15 +135,24 @@
 <?php 
 /*-----Hasil----*/
           echo '<div align=center>';
+          
           echo  $angka1.' '.$operasi.' '.$angka2.' = '.$hasil;
           echo '<br>';
           echo '==========================================================';
           echo '<br>';
+          echo 'Hasil Perhitungan ';
+          echo terbilang($angka1);
+          echo $op_terbilang;
+          echo terbilang($angka2);
+          echo " = ";
           echo terbilang($hasil);
+
           echo '<br>';
           echo '==========================================================';
           echo '<br>';
           echo '<input onclick="play();" type="button" value="🔊 Play" /></input>';
+          echo '<br>';
+          echo '<a href="">Kembali</a>';
           echo '</div> ';
          }
 
