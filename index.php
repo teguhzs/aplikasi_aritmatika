8f43a1b227035af36bc19ec4d660d060b05020e4
teguhzs <teguhs050196@gmail.com>
2018-08-08 19:18:02 +0700

    Aplikasi aritmatika fixed bilangan berkoma

--- index.php
+++ index.php
@@ -79,42 +79,84 @@
 
  /*-----------Fungsi Hasil terbilang...........*/ 
  
-  function penyebut($nilai) {
-    $nilai = abs($nilai);
-    $huruf = array("", "Satu", "Dua", "Tiga", "Empat", "Lima", "Enam", "Tujuh", "Delapan", "Sembilan", "Sepuluh", "Sebelas");
-    $temp = "";
-    if ($nilai < 12) {
-      $temp = " ". $huruf[$nilai];
-    } else if ($nilai <20) {
-      $temp = penyebut($nilai - 10). " Belas";
-    } else if ($nilai < 100) {
-      $temp = penyebut($nilai/10)." Puluh". penyebut($nilai % 10);
-    } else if ($nilai < 200) {
-      $temp = " Seratus" . penyebut($nilai - 100);
-    } else if ($nilai < 1000) {
-      $temp = penyebut($nilai/100) . " Ratus" . penyebut($nilai % 100);
-    } else if ($nilai < 2000) {
-      $temp = " Seribu" . penyebut($nilai - 1000);
-    } else if ($nilai < 1000000) {
-      $temp = penyebut($nilai/1000) . " Ribu" . penyebut($nilai % 1000);
-    } else if ($nilai < 1000000000) {
-      $temp = penyebut($nilai/1000000) . " Juta" . penyebut($nilai % 1000000);
-    } else if ($nilai < 1000000000000) {
-      $temp = penyebut($nilai/1000000000) . " Milyar" . penyebut(fmod($nilai,1000000000));
-    } else if ($nilai < 1000000000000000) {
-      $temp = penyebut($nilai/1000000000000) . " Trilyun" . penyebut(fmod($nilai,1000000000000));
-    }     
-    return $temp;
+function konversi($x){
+  
+  $x = abs($x);
+  $angka = array ("","Satu", "Dua", "Tiga", "Empat", "Lima", "Enam", "Tujuh", "Delapan", "Sembilan", "Sepuluh", "Sebelas");
+  $temp = "";
+  
+  if($x < 12){
+   $temp = " ".$angka[$x];
+  }else if($x<20){
+   $temp = konversi($x - 10)." Belas";
+  }else if ($x<100){
+   $temp = konversi($x/10)." Puluh". konversi($x%10);
+  }else if($x<200){
+   $temp = " Seratus".konversi($x-100);
+  }else if($x<1000){
+   $temp = konversi($x/100)." Ratus".konversi($x%100);   
+  }else if($x<2000){
+   $temp = " Seribu".konversi($x-1000);
+  }else if($x<1000000){
+   $temp = konversi($x/1000)." Ribu".konversi($x%1000);   
+  }else if($x<1000000000){
+   $temp = konversi($x/1000000)." Juta".konversi($x%1000000);
+  }else if($x<1000000000000){
+   $temp = konversi($x/1000000000)." Milyar".konversi($x%1000000000);
   }
+  
+  return $temp;
+ }
+  
+ function tkoma($x){
+  $str = stristr($x,",");
+  $ex = explode(',',$x);
+  
+  if(($ex[1]/10) >= 1){
+   $a = abs($ex[1]);
+  }
+  $string = array("Nol", "Satu", "Dua", "Tiga", "Empat", "Lima", "Enam", "Tujuh", "Delapan",   "Sembilan","Sepuluh", "Sebelas");
+  $temp = "";
  
-  function terbilang($nilai) {
-    if($nilai<0) {
-      $hasil = "minus ". trim(penyebut($nilai));
-    } else {
-      $hasil = trim(penyebut($nilai));
-    }         
-    return $hasil;
+  $a2 = $ex[1]/10;
+  $pjg = strlen($str);
+  $i =1;
+    
+  
+  if($a>=1 && $a< 12){   
+   $temp .= " ".$string[$a];
+  }else if($a>12 && $a < 20){   
+   $temp .= konversi($a - 10)." Belas";
+  }else if ($a>20 && $a<100){   
+   $temp .= konversi($a / 10)." Puluh". konversi($a % 10);
+  }else{
+   if($a2<1){
+    
+    while ($i<$pjg){     
+     $char = substr($str,$i,1);     
+     $i++;
+     $temp .= " ".$string[$char];
+    }
+   }
+  }  
+  return $temp;
+ }
+ 
+ function terbilang($x){
+  if($x<0){
+   $hasil = "Minus ".trim(konversi(x));
+  }else{
+   $poin = trim(tkoma($x));
+   $hasil = trim(konversi($x));
   }
+  
+if($poin){
+   $hasil = $hasil." Koma ".$poin;
+  }else{
+   $hasil = $hasil;
+  }
+  return $hasil;  
+ }
  
         
           
@@ -123,7 +165,7 @@
 <script type="text/javascript">
   function play (){
    responsiveVoice.speak(
-    "Hasil perhitungan <?php  echo  $angka1.' '.$op_terbilang.' '.$angka2.' = '.$hasil; ?>",
+    "Hasil perhitungan <?php  echo  $angka1.' '.$op_terbilang.' '.$angka2.' Adalah '.number_format($hasil,'2',',',''); ?>",
     "Indonesian Female",
    );
   }
@@ -136,7 +178,7 @@
 /*-----Hasil----*/
           echo '<div align=center>';
           
-          echo  $angka1.' '.$operasi.' '.$angka2.' = '.$hasil;
+          echo  $angka1.' '.$operasi.' '.$angka2.' = '.number_format($hasil,'2',',','');
           echo '<br>';
           echo '==========================================================';
           echo '<br>';
@@ -144,8 +186,8 @@
           echo terbilang($angka1);
           echo $op_terbilang;
           echo terbilang($angka2);
-          echo " = ";
-          echo terbilang($hasil);
+          echo " Adalah ";
+          echo ucwords(terbilang(number_format($hasil,'2',',','')));
 
           echo '<br>';
           echo '==========================================================';
